18d38550be02f6d6873d4bd560dd0c977d8e80dd
Devidpatle <96178882+Devidpatle@users.noreply.github.com>
2023-12-22 13:26:22 +0530

    Navbar style

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import "./navbar.css"
+
+import React from 'react';
+import './navbar.css';
 
 function Navbar() {
-    return (
-        <div className='Navbar'>
-            <div className="title"><h2>Title</h2></div>
-            <div className="links">
-                <li>Home</li>
-                <li>About</li>
-                <li>Blog</li>
-                <li>Contact</li>
-                <button className='btn'>Button</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className='Navbar'>
+      <div className='title'>
+        <h2>Title</h2>
+      </div>
+      <div className='links'>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+        <button className='btn'>Button</button>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
